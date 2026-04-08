9f5d876c8c68856f94afd27b015977fd08924e08
officialgaia <naitoworld@gmail.com>
2026-04-08 19:17:29 +0900

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,6 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-950">
       <Header />
-      <AdPlaceholder variant="banner" className="bg-gray-900/30 border-b border-gray-800" />
       <main className="flex-1">
         {children}
       </main>
